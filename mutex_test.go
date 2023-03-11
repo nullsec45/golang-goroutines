cf7a9b9ef010707da4f913ba4be14b6fa27f920a
explodesecurity45 <ramafajar805@gmail.com>
2023-03-11 17:42:27 +0700

    mutex

---
+++ mutex_test.go
@@ -0,0 +1,24 @@
+package go_routines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMutexRaceCondition(t *testing.T) {
+	var x = 0
+	var mutex sync.Mutex
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter =", x)
+}
