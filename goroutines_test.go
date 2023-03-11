26d4ec7e29af0def4d5bd884a6542d7c138ba3b0
explodesecurity45 <ramafajar805@gmail.com>
2023-03-11 17:42:19 +0700

    goroutines

---
+++ goroutines_test.go
@@ -0,0 +1,29 @@
+package go_routines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestCreateGoRoutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Ups")
+
+	time.Sleep(1 * time.Second)
+}
+
+func TestManyGoRoutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
+}
