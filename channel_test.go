1ecceb5179223e8ea9da2d07c26a97cc1b07de74
explodesecurity45 <ramafajar805@gmail.com>
2023-03-11 17:41:43 +0700

    channel

---
+++ channel_test.go
@@ -0,0 +1,160 @@
+package go_routines
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+
+	defer close(channel)
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Rama Fajar Fadhillah"
+		fmt.Println("Selesai mengirim data")
+	}()
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Rama Fajar Fadhillah"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(3 * time.Second)
+	close(channel)
+}
+
+// Untuk mengirim
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	//data := <-channel
+	channel <- "Rama Fajar Fadhillah"
+}
+
+// Untuk menerima
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	//channel <- "Rama"
+	//channel <- "Fajar"
+	//channel <- "Fadhillah"
+	//
+	//fmt.Println(<-channel)
+	//fmt.Println(<-channel)
+	//fmt.Println(<-channel)
+
+	//Pakai go routines
+	go func() {
+		channel <- "Rama"
+		channel <- "Fajar"
+		channel <- "Fadhillah"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
